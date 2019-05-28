b154e8ee3d55840d3b32c796c88d240aa02932e2
MinhoLee <alsgkgk777@naver.com>
2019-05-28 15:33:42 -0500

    markdown

---
+++ src/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDom from 'react-dom';
+
+
+const App = () => {
+    return (
+        <div>Hi</div>
+    );
+};
+
+
+ReactDom.render(
+    <App />,
+    document.querySelector('#root')
+);
