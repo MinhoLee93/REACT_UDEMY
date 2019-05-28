baca5093be6a678379e9036b2cf297df931ee52e
MinhoLee <alsgkgk777@naver.com>
2019-05-27 21:49:09 -0500

    03. lecture finished

---
+++ src/03.index.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import ReactDom from 'react-dom';
+//import faker from 'faker';
+import CommentDetail from './CommentDetail';
+import ApprovalCard from './ApprovalCard';
+
+const App = () => {
+    return (
+        <div className="ui container comments">
+            <ApprovalCard>
+                <CommentDetail author="Sam" timeAgo="Today at 4:45PM" content="Nice blog post" />
+            </ApprovalCard>
+            <ApprovalCard>
+                <CommentDetail author="Alex" timeAgo="Today at 2:00PM" content="Nice blog post2" />
+            </ApprovalCard>
+            <ApprovalCard>
+                <CommentDetail author="Jane" timeAgo="Yesterday at 5:00PM" content="Nice blog post3" />
+            </ApprovalCard>
+        </div >
+    );
+};
+
+ReactDom.render(
+    <App />,
+    document.querySelector('#root')
+);
