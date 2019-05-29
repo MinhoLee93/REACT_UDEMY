370a37312b336259fbd37c1aa4b41c58d6ae5727
MinhoLee <alsgkgk777@naver.com>
2019-05-29 09:44:43 -0500

    repeat 04.lecture

--- src/index.js
+++ src/index.js
@@ -1,50 +1,36 @@
 import React from 'react';
 import ReactDom from 'react-dom';
 
-/* functional component
-const App = () => {
-    window.navigator.geolocation.getCurrentPosition(
-        (position) => console.log(position),
-        (err) => console.log(err)
-    );
-
-    return (
-        <div>Latitute: </div>
-    );
-};
-*/
-
-// class component
+// App 클래스
 class App extends React.Component {
     constructor(props) {
+        // props를 parent에 전달
         super(props);
 
-        // this is the only time we do direct assignment
-        // to this.state
+        // state 초기화
         this.state = { lat: null };
 
+        // 비동기함수 호출
         window.navigator.geolocation.getCurrentPosition(
             (position) => {
+                // state 업데이트
                 this.setState({ lat: position.coords.latitude });
             },
-            (err) => console.log(err)
+            (err) => {
+                // 에러 호출
+                console.log(err);
+            }
         );
     }
 
-    // React says we have to define render!!
+    // render 함수 호출 / setState 결과값 return 후 rerender.
     render() {
-        /* render 함수는 frequency하게 호출되기때문에, 
-        여기에 호출함수를 작성하는 것보다 constructor에 넣어둬야한다.
-        window.navigator.geolocation.getCurrentPosition(
-            (position) => console.log(position),
-            (err) => console.log(err)
-        );
-        */
-
-        return <div>Latitude: {this.state.lat}</div>
+        // JSX 리턴
+        return <div>latitude: {this.state.lat}</div>
     }
-};
+}
 
+// 화면에 render
 ReactDom.render(
     <App />,
     document.querySelector('#root')
